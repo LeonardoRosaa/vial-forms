2c3e13afbe15efb0fce9cce29af82ef807d30ed1
Leonardo <leonardoorosaa16@gmail.com>
2025-04-14 11:06:44 -0300

    add serveless cors

--- backend/src/serveless.ts
+++ backend/src/serveless.ts
@@ -1,18 +1,18 @@
 import Fastify from 'fastify';
 import app from './routes'
+import CORSConfig from '@fastify/cors'
 
 const server = Fastify({
   logger: true
 }
 );
 
+server.register(CORSConfig, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+})
 server.register(app)
 
-// server.get('/', async (request, reply) => {
-//   return { message: 'Hello from Fastify on Vercel!' };
-// });
-
-// Main handler for Vercel
 export default async function handler(req, res) {
   await server.ready();
   server.server.emit('request', req, res);
